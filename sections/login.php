e6913e2217c7b8d99c8a69227deea72f3713e08e
David Pacassi Torrico <dpacassi@gmail.com>
2017-09-16 00:57:13 +0200

    Add log in form markup

--- sections/login.php
+++ sections/login.php
@@ -1,5 +1,21 @@
 <section class="login">
   <div class="content">
     <h2>Log in</h2>
+
+    <form id="user-login-form" class="user-login-form" method="post" accept-charset="UTF-8">
+      <div class="form-item">
+        <label for="username">Username</label>
+        <input id="username" type="text" name="username" value="" />
+      </div>
+
+      <div class="form-item">
+        <label for="password">Password</label>
+        <input id="password" type="password" name="password" value="" />
+      </div>
+
+      <div class="form-actions">
+        <input id="submit" type="submit" value="Log in" class="button button-primary" />
+      </div>
+    </form>
   </div>
 </section>
