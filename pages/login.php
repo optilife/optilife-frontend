d6275b7645ee1e903beadcca3cd9a3866fb7742a
David Pacassi Torrico <dpacassi@gmail.com>
2017-09-16 15:39:57 +0200

    Add minor UX enhancements

--- pages/login.php
+++ pages/login.php
@@ -3,17 +3,21 @@
     <h1>Log in</h1>
 
     <form id="user-login-form" class="user-login-form" method="post" accept-charset="UTF-8">
-      <div class="form-item">
-        <input id="username" type="text" name="username" value="" />
-        <label for="username">Username</label>
-        <span class="focus-border"></span>
-      </div>
+      <fieldset>
+        <legend>Optimize your life!</legend>
 
-      <div class="form-item">
-        <input id="password" type="password" name="password" value="" />
-        <label for="password">Password</label>
-        <span class="focus-border"></span>
-      </div>
+        <div class="form-item">
+          <input id="username" type="text" name="username" value="" />
+          <label for="username">Username</label>
+          <span class="focus-border"></span>
+        </div>
+
+        <div class="form-item">
+          <input id="password" type="password" name="password" value="" />
+          <label for="password">Password</label>
+          <span class="focus-border"></span>
+        </div>
+      </fieldset>
 
       <div class="form-actions">
         <input id="submit" type="submit" value="Log in" class="button button-primary" />
--- pages/upload.php
+++ pages/upload.php
@@ -1,12 +1,16 @@
 <section class="upload">
   <div class="content">
-    <h1>Upload</h1>
+    <h1>Add data</h1>
 
     <form id="user-upload-form" class="user-upload-form" method="post" accept-charset="UTF-8">
-      <div class="form-item form-item__standard">
-        <label for="image">Image</label>
-        <input id="image" type="file" name="image" value="" />
-      </div>
+      <fieldset>
+        <legend>Add food or receipt images</legend>
+
+        <div class="form-item form-item__standard">
+          <label for="image">Image</label>
+          <input id="image" type="file" name="image" value="" />
+        </div>
+      </fieldset>
 
       <div class="form-actions">
         <input id="submit" type="submit" value="Upload" class="button button-primary" />
